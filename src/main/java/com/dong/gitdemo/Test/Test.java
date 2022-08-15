e23851986d567ee09b0a61953172296b57a3ae2f
sigonghuiye <sigonghuiye@qq.com>
2022-08-15 17:53:28 +0800

    sigonghuiye commit-2

--- src/main/java/com/dong/gitdemo/Test/Test.java
+++ src/main/java/com/dong/gitdemo/Test/Test.java
@@ -4,10 +4,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("11111");
-        System.out.println("22222");
+        System.out.println("sigonghuiye update");
         System.out.println("33333");
-        System.out.println("44444");
-        System.out.println("git sigonghuiye");
     }
 
 }
