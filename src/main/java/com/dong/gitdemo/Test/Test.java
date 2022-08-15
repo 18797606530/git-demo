cf65abf0b177187de6232282afb0fe08aa2c0e1b
sigonghuiye <sigonghuiye@qq.com>
2022-08-15 16:21:20 +0800

    send commit

--- src/main/java/com/dong/gitdemo/Test/Test.java
+++ src/main/java/com/dong/gitdemo/Test/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("22222");
-        System.out.println("55555");
+        System.out.println("666");
     }
 
 }
