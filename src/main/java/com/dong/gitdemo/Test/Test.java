251976079fb1dbc59a4333faf9b4538f281cf7da
sigonghuiye <sigonghuiye@qq.com>
2022-08-15 16:49:36 +0800

    第六次提交

--- src/main/java/com/dong/gitdemo/Test/Test.java
+++ src/main/java/com/dong/gitdemo/Test/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("四宫辉夜");
         System.out.println("darling");
+        System.out.println("123456");
     }
 
 }
