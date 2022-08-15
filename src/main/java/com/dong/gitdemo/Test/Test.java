ba2656357db9a3cd9d66a75fffea8157a74feef4
sigonghuiye <sigonghuiye@qq.com>
2022-08-15 16:36:58 +0800

    第五次提交

--- src/main/java/com/dong/gitdemo/Test/Test.java
+++ src/main/java/com/dong/gitdemo/Test/Test.java
@@ -3,11 +3,7 @@ package com.dong.gitdemo.Test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("11111");
-        System.out.println("22222");
-        System.out.println("666");
-        System.out.println("123456");
-        System.out.println("8888");
+        System.out.println("四宫辉夜");
     }
 
 }
