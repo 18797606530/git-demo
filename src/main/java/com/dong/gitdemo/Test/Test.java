b07c4969367771f83972c2e7788f415cc9ba2d39
sigonghuiye <sigonghuiye@qq.com>
2022-08-15 16:14:13 +0800

    first commit

---
+++ src/main/java/com/dong/gitdemo/Test/Test.java
@@ -0,0 +1,12 @@
+package com.dong.gitdemo.Test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
+        System.out.println("44444");
+    }
+
+}
